c1e3e39a65fdeecaf36f1b336c857d0d202a2e5e
Niyathi A S <niyathy@gmail.com>
2022-06-22 17:31:06 +0530

    Initial commit for ansible shell script

---
+++ automation/ansible/ansible.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+cd /tmp
+rm -rf cloud-monitoring-platform
+git clone https://github.com/niyaCode/cloud-monitoring-platform.git
+cd /home/ansibleadmin/
+rm -rf ansible
+mkdir ansible
+cd ansible
+cp /tmp/export-variables.sh /tmp/inventory.cfg .
+
+cp /tmp/cloud-monitoring-platform/automation/ansible/cmp.yml .
+cp -r /tmp/cloud-monitoring-platform/automation/ansible/roles .
+. export-variables.sh
+echo $ENV
+# echo $CLIENT_ID
+# echo $CLIENT_SECRET
+# echo $TENANT_ID
+ansible-playbook -i inventory.cfg cmp.yml
+
